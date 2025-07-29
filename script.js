7cfcc346635458f97ad1674f00b57d7ef1021cd3
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-07-29 07:12:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+function clickHandler() {
+	const para = document.getElementById("status");
+
+	const h1 = document.createElement("h1");
+	h1.textContent = "Entered Metaverse";
+
+	para.replaceWith(h1);
+}
